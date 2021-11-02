fa7867419ef4bcd37c4f39f6c9c33356cd1da97b
Sam Phillips <samdphillips@gmail.com>
2021-11-02 13:56:39 -0700

    Add web-server-lib as dep

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define name "raco-static-web")
 (define collection "raco-static-web")
 (define version "0.9.0")
-(define deps '("base"))
+(define deps '("base" "web-server-lib"))
 (define build-deps '())
 (define pkg-authors '(samdphillips@gmail.com))
 (define raco-commands
