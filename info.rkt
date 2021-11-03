ffc59d96f34b5e4372a2399cbb87d721400b9751
Sam Phillips <samdphillips@gmail.com>
2021-11-03 14:14:31 -0700

    Overhauled

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 
 (define name "raco-static-web")
 (define collection "raco-static-web")
-(define version "0.9.0")
+(define version "0.9.2")
 (define deps '("base" "web-server-lib"))
 (define build-deps '())
 (define pkg-authors '(samdphillips@gmail.com))
--- main.rkt
+++ main.rkt
@@ -1,45 +1,77 @@
 #lang racket/base
 
-(require web-server/servlet
-         web-server/servlet-env
+(require (for-syntax racket/base)
+         net/url
          racket/path
-         racket/string
-         racket/list)
+         racket/runtime-path
+         (prefix-in log: web-server/dispatchers/dispatch-log)
+         (prefix-in sequencer: web-server/dispatchers/dispatch-sequencer)
+         (prefix-in static-files: web-server/dispatchers/dispatch-files)
+         (prefix-in lift: web-server/dispatchers/dispatch-lift)
+         web-server/configuration/responders
+         web-server/dispatchers/dispatch
+         web-server/dispatchers/filesystem-map
+         web-server/http/request-structs
+         web-server/http/xexpr
+         web-server/web-server)
 
-(define servlet-path "/static-files")
-(define segments-to-drop (length (string-split servlet-path "/")))
+(define-runtime-path web-server-collection '(lib "web-server"))
+
+(define (relative-path-url-to-root p)
+  (define simple-path (simplify-path p))
+  (define rel-path (find-relative-path (current-directory) simple-path))
+  (cond
+    [(equal? simple-path rel-path) (url #f #f #f #f #t null null #f)]
+    [else
+      (define pp
+        (for/list ([d (in-list (explode-path rel-path))])
+          (path/param (path->string d) null)))
+      (url #f #f #f #f #t pp null #f)]))
+
+(define (make-file-link path)
+  (define basename
+    (path->string
+      (file-name-from-path path)))
+  (define rel-to-pwd
+    (find-relative-path (current-directory) path))
+  `(li (a ([href ,rel-to-pwd]) ,basename)))
 
 (define (files-list path)
-  (define files (directory-list path #:build? #t))
-  (define links
-    (map (λ (path)
-           (let* ([basename (file-name-from-path path)]
-                  [basename-text (path->string basename)]
-                  [rel-to-pwd (find-relative-path (current-directory) path)]
-                  [dir-link (path->string (build-path servlet-path rel-to-pwd))]
-                  [file-link (string-append "/" (path->string rel-to-pwd))])
-             `(li (a ([href ,(if (directory-exists? path) dir-link file-link)])
-                     ,basename-text))))
-         files))
-  `(ul ,@links))
+  (for/list ([f (directory-list path #:build? #t)])
+    (define name (path->string (file-name-from-path f)))
+    (define u (relative-path-url-to-root f))
+    `(li (a ([href ,(url->string u)]) ,name))))
 
-(serve/servlet
-  (λ (req)
-    (define path-segments
-      (map path/param-path
-           (drop (url-path (request-uri req)) segments-to-drop)))
-    (define file-or-dir (apply build-path (current-directory) path-segments))
-    (if (directory-exists? file-or-dir)
+(define (directory-lister:make #:url->path url->path)
+  (lift:make
+    (lambda (req)
+      (define-values (path pieces) (url->path (request-uri req)))
+      (unless (directory-exists? path)
+        (next-dispatcher))
       (response/xexpr
-        `(html (body
-                 (h1 ,(path->string file-or-dir))
-                 ,(files-list file-or-dir))))
-      (response/xexpr
-        #:code 404
-        `(html (body
-                 (h1 ,(path->string file-or-dir) " Not found"))))))
-  #:launch-browser? #f
-  #:extra-files-paths (list (current-directory))
-  #:servlet-path servlet-path
-  #:servlet-regexp (regexp (format "^~a" (regexp-quote servlet-path)))
-  #:servlet-current-directory (current-directory))
+        `(html
+           (body
+             (h1 ,(url->string (request-uri req)))
+             ,@(files-list path)))))))
+
+(define url->path
+  (make-url->path (current-directory)))
+
+(void
+  (serve #:port 8000
+         #:dispatch
+         (sequencer:make
+           (log:make #:format
+                     (log:log-format->format 'apache-default)
+                     #:log-path (current-output-port))
+           (static-files:make #:url->path url->path)
+           (directory-lister:make #:url->path url->path)
+           (lift:make
+             (gen-file-not-found-responder
+               (simplify-path
+                 (build-path web-server-collection 'up
+                             "default-web-root" "conf"
+                             "not-found.html")))))))
+
+(with-handlers ([exn:break? void]) (do-not-return))
+
