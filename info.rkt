595c2272d55181ca14b17c5827140b900dc78f9e
Sam Phillips <samdphillips@gmail.com>
2022-11-11 14:02:33 -0800

    Add license.  Closes #8

--- info.rkt
+++ info.rkt
@@ -3,14 +3,13 @@
 (define name "raco-static-web")
 (define collection "raco-static-web")
 (define version "1.0.1")
-(define deps '("base"
-               "web-server-lib"
-               "mime-type-lib"
-               "version-case"))
+(define deps '("base" "web-server-lib" "mime-type-lib" "version-case"))
 (define build-deps '())
 (define pkg-authors '(samdphillips@gmail.com))
+(define license 'Apache-2.0)
 (define raco-commands
-  '(("static-web"
+  '(["static-web"
      (submod raco-static-web main)
      "runs a webserver serving files from the given directory"
-     10)))
+     10]))
+
