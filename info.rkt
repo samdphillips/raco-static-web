41e04a9905142d6d9102147df30021161ae3c5ae
Joel Dueck <joel@jdueck.net>
2022-12-30 09:24:47 -0600

    Update dependencies

--- info.rkt
+++ info.rkt
@@ -3,7 +3,8 @@
 (define name "raco-static-web")
 (define collection "raco-static-web")
 (define version "1.0.1")
-(define deps '("base" "web-server-lib" "mime-type-lib" "version-case"))
+(define deps '("net-lib"
+               "base" "web-server-lib" "mime-type-lib" "version-case"))
 (define build-deps '())
 (define pkg-authors '(samdphillips@gmail.com))
 (define license 'Apache-2.0)
